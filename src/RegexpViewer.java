75cfc1e57f8692c18552965f8ac46d09ba582556
LittleD <pisz.na@gmail.com>
2011-08-05 16:09:42 +0200

    stable

--- src/RegexpViewer.java
+++ src/RegexpViewer.java
@@ -5,15 +5,19 @@ import java.util.regex.*;
 
 class MainWindow extends Ui_MainWindow {
 	void process() {
-		Pattern wiersz_p = Pattern.compile(LE_regexp.text());
-		System.out.println(TE_in.toPlainText());
-		Matcher wiersz_m = wiersz_p.matcher(TE_in.toPlainText());
-		while (wiersz_m.find() != false) {
-			TE_out.append("Match: "+ wiersz_m);
-			for (int i = 0; i < wiersz_m.groupCount(); i++) {
-				TE_out.append(i+" "+wiersz_m.group(i)+"/n");
+		try {
+			Pattern wiersz_p = Pattern.compile(LE_regexp.text());
+			Matcher wiersz_m = wiersz_p.matcher(TE_in.toPlainText());
+			TE_out.clear();
+			int match_count = 0;
+			while (wiersz_m.find() != false) {
+				TE_out.append("Match "+ (match_count++) +": "+ wiersz_m.group());
+				for (int i = 1; i <= wiersz_m.groupCount(); i++) {
+					TE_out.append("\t"+i+": "+wiersz_m.group(i));
+				}
 			}
 		}
+		catch(PatternSyntaxException e) { TE_out.clear();TE_out.append(e.getMessage()); }
 	}
 	void setupNext() {
 		PB_process.clicked.connect(this,"process()");
--- src/Ui_MainWindow.java
+++ src/Ui_MainWindow.java
@@ -11,8 +11,9 @@ import com.trolltech.qt.gui.*;
 public class Ui_MainWindow implements com.trolltech.qt.QUiForm<QMainWindow>
 {
     public QWidget centralwidget;
-    public QWidget layoutWidget;
     public QVBoxLayout verticalLayout;
+    public QHBoxLayout horizontalLayout;
+    public QLabel label;
     public QLineEdit LE_regexp;
     public QTextEdit TE_in;
     public QTextEdit TE_out;
@@ -25,31 +26,38 @@ public class Ui_MainWindow implements com.trolltech.qt.QUiForm<QMainWindow>
     public void setupUi(QMainWindow MainWindow)
     {
         MainWindow.setObjectName("MainWindow");
-        MainWindow.resize(new QSize(605, 437).expandedTo(MainWindow.minimumSizeHint()));
+        MainWindow.resize(new QSize(631, 702).expandedTo(MainWindow.minimumSizeHint()));
         centralwidget = new QWidget(MainWindow);
         centralwidget.setObjectName("centralwidget");
-        layoutWidget = new QWidget(centralwidget);
-        layoutWidget.setObjectName("layoutWidget");
-        layoutWidget.setGeometry(new QRect(0, 0, 602, 392));
-        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout = new QVBoxLayout(centralwidget);
         verticalLayout.setObjectName("verticalLayout");
-        LE_regexp = new QLineEdit(layoutWidget);
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout.setObjectName("horizontalLayout");
+        label = new QLabel(centralwidget);
+        label.setObjectName("label");
+
+        horizontalLayout.addWidget(label);
+
+        LE_regexp = new QLineEdit(centralwidget);
         LE_regexp.setObjectName("LE_regexp");
 
-        verticalLayout.addWidget(LE_regexp);
+        horizontalLayout.addWidget(LE_regexp);
 
-        TE_in = new QTextEdit(layoutWidget);
+
+        verticalLayout.addLayout(horizontalLayout);
+
+        TE_in = new QTextEdit(centralwidget);
         TE_in.setObjectName("TE_in");
 
         verticalLayout.addWidget(TE_in);
 
-        TE_out = new QTextEdit(layoutWidget);
+        TE_out = new QTextEdit(centralwidget);
         TE_out.setObjectName("TE_out");
         TE_out.setMinimumSize(new QSize(300, 0));
 
         verticalLayout.addWidget(TE_out);
 
-        PB_process = new QPushButton(layoutWidget);
+        PB_process = new QPushButton(centralwidget);
         PB_process.setObjectName("PB_process");
 
         verticalLayout.addWidget(PB_process);
@@ -57,7 +65,7 @@ public class Ui_MainWindow implements com.trolltech.qt.QUiForm<QMainWindow>
         MainWindow.setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar.setObjectName("menubar");
-        menubar.setGeometry(new QRect(0, 0, 605, 23));
+        menubar.setGeometry(new QRect(0, 0, 631, 20));
         MainWindow.setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar.setObjectName("statusbar");
@@ -70,6 +78,7 @@ public class Ui_MainWindow implements com.trolltech.qt.QUiForm<QMainWindow>
     void retranslateUi(QMainWindow MainWindow)
     {
         MainWindow.setWindowTitle(com.trolltech.qt.core.QCoreApplication.translate("MainWindow", "MainWindow", null));
+        label.setText(com.trolltech.qt.core.QCoreApplication.translate("MainWindow", "Regexp:", null));
         PB_process.setText(com.trolltech.qt.core.QCoreApplication.translate("MainWindow", "Process", null));
     } // retranslateUi
 
